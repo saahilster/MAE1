69175d760f80804affd3f9fa19c929cd60e6703d
saahil <farooksaahil@gmail.com>
2026-06-25 22:55:38 -0700

    working on decoder

--- proj/components/decoder/decoder.cpp
+++ proj/components/decoder/decoder.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "decoder.h"
+#include <stdbool.h>
 #include "esp_heap_caps.h"
 #include "micro_flac/flac_decoder.h"
 
@@ -8,9 +9,17 @@ FLACDecoder dec;
 
 void decode_song(const char* filePath){
 
+    bool have_data = true;
+
     FILE* fptr = fopen(filePath, "rb");
     fseek(fptr, 0, SEEK_END);
-    size_t input_len = ftell(fptr);
+
+
+
+    //input_len should be chunked for reading
+    //set to 4MiB
+    size_t input_len = 4 * 1024 * 1024;
+
     rewind(fptr);
 
     if (fptr == NULL){
@@ -21,4 +30,9 @@ void decode_song(const char* filePath){
     uint8_t* input = (uint8_t*)heap_caps_malloc(input_len, MALLOC_CAP_SPIRAM);
     fread(input, 1, input_len, fptr);
     fclose(fptr);
+
+    //function to decode song
+    while(have_data){
+
+    }
 }
